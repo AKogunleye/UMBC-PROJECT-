4e60acd9dff9605b2d0a88c879a99a05a3872fca
AK  = 'python <>
2022-06-28 21:32:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Display the Delarship name with a marketable friendly greetings 
+print("Welcome to PeaceCar \n Customization Form! ") 
+# aked the customer "the kind of car of intrest?!"
+print("What car make & model are you ordering")
+# To better assist you , customer enter from the multiple choice letter for your choice of make and model.
+print ("a.Lighthig\n b.Eco Leaf\n c.Harp chord\n d.ChevronJolt")
+#Customer pick from the option from  (A to D )
+modelOption = input("'a' 'b'c'd:")
+print(modelOption)
+#ask customer for upgrade option?
+doorOption = input("would you like to upgrade to a 2- door option?")
+# asking customer for Exterior choices ?
+colorOption = input("what color would you like your car to be ?")
+deluxeWeatherOption = input("would you like the deluxe weather package?yes or no :")
+# Asking customer for Interior option
+print("a.I-4 Entry Engine\n b.V-6 Performance Engine\n c. Eco Diesel Engine.")
+engineOption = input("a.b.c.")
+heatedSeats =("would you like to heated seats?")
+# Display the sumarry of the customer option?
+print("===================")
+print("~Summary~")
+print(f"Model Option:{modelOption}")
+print(f"Upgrade to 2-Door:{doorOption}")
+print(f"Desired Color:{colorOption}")
+print(f"Upgrade to Deluxe Weather:{deluxeWeatherOption}")
+print(f"Engine Option:{engineOption}")
+print(f"Upgrade to Heated Seats:{heatedSeats}")
